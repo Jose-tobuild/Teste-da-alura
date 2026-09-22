e7bc6a7f7551ff63b513d619721f8776e3a4421f
Jose-tobuild <joseluis161204@gmail.com>
2026-09-22 07:58:07 -0300

    Add files via upload

---
+++ app - Copy.js
@@ -0,0 +1,54 @@
+/*
+alert('Welcome to the gameee');
+let SecretNumber = 104;
+console.log(SecretNumber);
+let guess = prompt('Guess the Screcret Number');
+
+//se chute for igual ao número secreto
+
+if(SecretNumber == guess){
+    alert(`Congrats! You guessed the secret number ${SecretNumber}`);
+} else {
+    alert('sorry you didnt get the answer');
+}
+*/
+
+
+//3 concatenação apóstrofo 
+
+//testando o game dos cupons
+/*
+let quantidademilhas = prompt('Quantas milhas você possúi?');
+if(quantidademilhas < 5000){
+    alert('você não tem desconto');
+}
+if(quantidademilhas >= 30000){
+    alert('Você tem desconto de 20%');
+}else if(quantidademilhas >= 5000){
+    alert('seu desconto é de 10%');
+}*/
+
+//adicionando texto junto da variável
+
+/*
+alert('Welcome to the secret number');
+let chute = prompt('choose a number between 1 and 10');
+console.log(`seu chute foi${chute}`);
+let secretnumber = 6;
+console.log(`the secret number is ${secretnumber}`)
+if(secretnumber == chute){
+    alert('congrats, you got the secret number');
+}else alert('You did nott get it kkkkkkkk');
+*/
+
+alert('Oh my gosh, what is a princess like you is doing here?');
+alert('Okay, lets play');
+alert('I am thinking in a number between 1 and 100 and you have to guess');
+let correctanswer = prompt('What is the number?');
+if (correctanswer >= 1){
+    alert('You got it salamattt, now if you dont send a picture for the next 5 minutes the whole universe will explode');
+} else {
+    alert('I am so sorry, this one is not a number')
+} alert('hehehe');
+console.log('You are beautiful');
+alert('another thing, press OK and then press the button F12 of your kayboard and check it, if something like DEV TOOLS showed up you can click on it, and maybe you will have to select the option CONSOLE too');
